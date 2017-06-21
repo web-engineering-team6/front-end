b5d771934093332e61db0d27180068407bd89c92
AkihikoIshikawa <aki.mailer@gmail.com>
2017-06-21 22:53:22 +0900

    make result.html

--- js/result.js
+++ js/result.js
@@ -1,1 +1,18 @@
 /* result.htmlのjsはココ */
+
+//ツイート送信
+function tweet(){
+	//ツイートを送る処理を書きます
+	//console.log("test-tweet");
+
+	return false;
+};
+
+
+//データSlack送信
+function slacksend(){
+	//Slackを送る処理を書きます
+	//console.log("test-slack");
+
+	return false;
+};
