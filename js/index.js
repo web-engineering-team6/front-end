37b8c3829a41dd51d33535d67baeb429b3f6a62f
AkihikoIshikawa <aki.mailer@gmail.com>
2017-06-21 22:35:24 +0900

    delete loading and make index.html

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 /* index.htmlのjsはココ */
+
+//画像切り取りAPI
+$(function(){
+	//画像データをAPIに送信する処理を書きます
+	// http://black-flag.net/jquery/20150217-5587.html このへんを参考に実装すれば良さそう
+});
+
+//データ送信API
+function picsend(){
+	//各種データをAPIに送信する処理を書きます
+
+    document.location.href = "result.html";
+	return false;
+};
+
+//ページの頭に戻る処理
+$(function(){
+	$('#pageTop').click(function(){
+	$('index,html').animate({
+	scrollTop: 0},500);
+	return false;
+	})
+});
