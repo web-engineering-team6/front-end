b1756887b67f7fc74ababebe591d13c7605dc630
tommygoji <goji@ut-ec.co.jp>
2017-06-28 02:41:47 +0900

    Goji's Revision on Wed, June 28
    Goji's revisions for Ishikawa-kun's review on Mon, June 26. Solved Ishikawa-kun's Comments 1, 2 and 4. Comment 3 is unresolved, which seems beyond Goji's current skill set.

--- js/result.js
+++ js/result.js
@@ -1,12 +1,29 @@
 /* result.htmlのjsはココ */
 
+// onButtonClick()をhtmlファイルからjsファイルに移動
+function onButtonClick() {
+  target1 = document.getElementById("output1");
+  target2 = document.getElementById("output2");
+  target3 = document.getElementById("output3");
+  target1.innerText = document.forms.id_form1.id_textBox1.value;
+  target2.innerText = document.forms.id_form2.id_textBox2.value;
+  target3.innerText = document.forms.id_form3.id_textBox3.value;
+}
+
 // 申込FormのtextBoxへの記入がConfirmされたものをSlackへPost
 $(function () {
     $('.slack-submit').on('click', function () {
     var url = 'https://slack.com/api/chat.postMessage';
 
+        // GETリクエストでtoken.jsのjavascriptファイルを読み込みコールバック関数で実行
+        // http://www.jiichan.com/programming/programming.php?lang=js&no=03
+        $.getScript("token.js", function(){
+          alert('Can I read token?');
+        });
+
         var data = {
-						token: 'xoxp--190466053957--189718136128--201686809458--3b59530140f3ae8d9b76852092f5771b',
+						token: token,
+            // ⬆︎ 別ファイルtoken.jsに書いたグローバル変数var token = 'xoxp-...'を呼び出す。
             // token: 上のtoken情報中の--を-に置換するとワークします。4箇所。GithubでそのままtokenをPublicに共有すると無効化されるため。',
             channel: '#yoshinobu-channel',
             username: 'yoshinobu-nisei-bot',
