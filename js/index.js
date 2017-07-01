53f59d782709f748ba503e049290e38bf88903ef
AkihikoIshikawa <aki.mailer@gmail.com>
2017-07-02 00:15:24 +0900

    test

--- js/index.js
+++ js/index.js
@@ -8,9 +8,38 @@ $(function(){
 
 //データ送信API
 function picsend(){
-	//各種データをAPIに送信する処理を書きます
+	var request = new XMLHttpRequest();
 
-    document.location.href = "result.html";
+	request.open('POST', 'https://private-anon-0b73dcc383-aichangesfaces.apiary-mock.com/v1/faceAnalysis');
+
+	request.setRequestHeader('Content-Type', 'application/json');
+
+	request.onreadystatechange = function () {
+	  if (this.readyState === 4) {
+	    console.log('Status:', this.status);
+	    console.log('Headers:', this.getAllResponseHeaders());
+ 	   	console.log('Body:', this.responseText);
+
+ 	   	var body = JSON.parse(this.responseText);
+
+ 	   	console.log(body);
+
+		console.log(body[0].attribute_name1);
+		console.log(body[0]);
+
+
+ 	 }
+	};
+
+	var body = {
+	    'image_url': '/static/img/000001.png',
+	    'analysis_type': 'seasoning'
+	};
+
+	request.send(JSON.stringify(body));
+
+
+    //document.location.href = "result.html";
 	return false;
 };
 
